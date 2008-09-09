fb2050d8bf774a9f5d2313a34db7a821590186a5
Mike Bailey <mike@bailey.net.au>
2008-09-09 22:03:50 +1000

    Build and install gem on all :dev machines with:
        cap bi

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,15 @@
 require 'deprec'
 
-role :dev, 'bb' 
+role :dev, 'bb', 'localhost'
 
 set :application, 'ruby-xen'
 set :version, '0.0.1'
+
+
+task :bi do
+  build
+  install
+end
 
 task :build do
   `gem build #{application}.gemspec`
