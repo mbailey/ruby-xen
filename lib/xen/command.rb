f0be21f6681104d3cbe0d1287b365e3e0c2d7c9a
Nicolas Marfleet <nick@nicko-2.local>
2008-09-11 23:26:56 +1000

    added some functionality around the first couple of command wrappers.  Some specs to go with

--- lib/xen/command.rb
+++ lib/xen/command.rb
@@ -1,8 +1,15 @@
 class Xen::Command
   def self.xm_list
-    `xm list`
+    puts 'HEE'*100
+    raw = `xm list`.scan(/(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/)
+    headers = raw.delete_at(0)
+    raw.enum_with_index.map do |row, i|
+      headers.inject({}) { |m, head| m[head] = row[i]; m  }
+    end
   end
+  
   def self.xm_info
-    `xm info`
+    result = `xm info`
+    result.scan(/(\S+)\s*:\s*([^\n]+)/).inject({}){ |m, (i,j)| m[i.to_sym] = j; m }
   end 
 end
