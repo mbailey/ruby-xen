1b5178da478e2669359dfe6997af7fcf370fc8f0
Mike Bailey <mike@bailey.net.au>
2008-09-15 22:36:20 +1000

    Commented out xm_list command as detailed_instance_list gives us all that +start_time. There shouldn't be any need to use xm_list so it may be removed in future.

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -6,7 +6,7 @@ rescue LoadError
   require 'spec'
 end
 
-$:.unshift(File.dirname(__FILE__), '..', '..', 'lib')
-require 'ruby-xen'
+# $:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+require File.dirname(__FILE__) + '/../../lib/ruby-xen'
 require File.join(File.dirname(__FILE__), 'xen_commands_helper')
 SPEC_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
