ee8370dea1ed1a541fc74636c9bb9ed8f1c677a8
Mike Bailey <mike@bailey.net.au>
2009-01-16 14:18:01 +1100

    Load info about bridged from Xen config files

--- lib/xen/host.rb
+++ lib/xen/host.rb
@@ -24,4 +24,41 @@ module Xen
   
   end
   
+  
+  # XXX Move this somewhere else!
+  
+  # Network Bridge Script looks like this.
+  #
+  # #!/bin/sh
+  # /etc/xen/scripts/network-bridge $1 netdev=eth0 bridge=xenbr0 vifnum=0 antispoof=no
+  # /etc/xen/scripts/network-bridge $1 netdev=eth1 bridge=xenbr1 vifnum=1 antispoof=no
+  
+  class Bridges
+    NETWORK_BRIDGE_WRAPPER = '/etc/xen/scripts/network-bridge-wrapper'
+    
+    def self.find
+      f = File.readlines(NETWORK_BRIDGE_WRAPPER).collect { |line|
+        if (m = line.match /netdev=(.*) bridge=(.*) vifnum=(.*) antispoof=(.*)/)
+          Xen::Bridge.new :netdev => m[1], :bridge => m[2], :vifnum => m[3], :antispoof => m[4]
+        end
+      }.compact
+    end
+    
+    def self.save
+    end
+    
+  end
+  
+  class Bridge
+    attr_accessor :netdev, :bridge, :vifnum, :antispoof
+    
+    def initialize(*args)
+      options = args.extract_options!
+      @netdev = options[:netdev]
+      @bridge = options[:bridge]
+      @vifnum = options[:vifnum]
+      @antispoof = options[:antispoof]
+    end
+  end
+  
 end
